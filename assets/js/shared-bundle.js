2b5311ff873ddfc9da380000d1c4a2638b96430b
bayareadiscounts <246928973+bayareadiscounts@users.noreply.github.com>
2025-12-20 19:57:16 -0800

    Fix duplicate export in shared-bundle.js

--- assets/js/shared-bundle.js
+++ assets/js/shared-bundle.js
@@ -209,14 +209,3 @@ async function translateTexts(options) {
 }
 
 module.exports = { translateTexts, DEFAULT_TRANSLATE_ENDPOINT };
-const { createCache } = require('./cache');
-const { ApiClient, DEFAULT_BASE_URL } = require('./api-client');
-const { translateTexts, DEFAULT_TRANSLATE_ENDPOINT } = require('./translation');
-
-module.exports = {
-  createCache,
-  ApiClient,
-  DEFAULT_BASE_URL,
-  translateTexts,
-  DEFAULT_TRANSLATE_ENDPOINT
-};
